b24f795dfc3f3ac9fbb93f4788f8bb55440b50d5
purelengend <nhathuy27301@gmail.com>
2022-12-19 09:54:28 +0700

    Add enpoints for services

--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,4 +1,5 @@
 using CloudWeather.Temperature.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,4 +13,22 @@ builder.Services.AddDbContext<TemperatureDbContext>(
 );
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, TemperatureDbContext db) => 
+{
+  if (days == null || days < 0 || days > 30) 
+    return Results.BadRequest("Invalid 'days' parameter. Please provide a value from 1 to 30.");
+  
+  var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+  var results = await db.Temperature.Where(temp => temp.ZipCode == zip && temp.CreatedOn > startDate).ToListAsync();
+
+  return Results.Ok(results);
+});
+
+app.MapPost("/observation", async (Temperature temp, TemperatureDbContext db) => 
+{
+  temp.CreatedOn = temp.CreatedOn.ToUniversalTime();
+  await db.Temperature.AddAsync(temp);
+  await db.SaveChangesAsync();
+});
 app.Run();
