fa590204374eec68f521fe70fa555e548003f07a
purelengend <nhathuy27301@gmail.com>
2022-12-19 09:21:33 +0700

    connect Precipitation Service to NPSQL

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,10 +1,26 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDbContext>(
+  opts => { 
+    opts.EnableSensitiveDataLogging();
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+  }, ServiceLifetime.Transient
+);
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => {
-  return Results.Ok(zip);
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) => 
+{
+  if (days == null || days < 0 || days > 30) 
+    return Results.BadRequest("Invalid 'days' parameter. Please provide a value from 1 to 30.");
+  
+  var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+  var results = await db.Precipitation.Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate).ToListAsync();
+
+  return Results.Ok(results);
 });
 app.Run();
