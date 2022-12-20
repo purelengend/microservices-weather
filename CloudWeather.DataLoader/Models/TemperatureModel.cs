e300ceb5ca0f31d5563c4dfe7dc7c5c14bb0f892
purelengend <nhathuy27301@gmail.com>
2022-12-20 09:53:47 +0700

    Build initial Data Loader

---
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -0,0 +1,9 @@
+namespace CloudWeather.DataLoader.Models;
+
+internal class TemperatureModel
+{
+   public DateTime CreatedOn { get; set; }
+   public decimal TempHighF { get; set; }
+   public decimal TempLowF { get; set; }
+   public string ZipCode { get; set; }
+}
--- CloudWeather.DataLoader/Program.cs
+++ CloudWeather.DataLoader/Program.cs
@@ -1,2 +1,139 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Json;
+using CloudWeather.DataLoader.Models;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+      .AddJsonFile("appSettings.json")
+      .AddEnvironmentVariables()
+      .Build();
+
+var servicesConfig = config.GetSection("Services");
+
+var tempServiceConfig = servicesConfig.GetSection("Temperature");
+var tempServiceHost = tempServiceConfig["Host"];
+var tempServicePort = tempServiceConfig["Port"];
+
+var precipServiceConfig = servicesConfig.GetSection("Precipitation");
+var precipServiceHost = precipServiceConfig["Host"];
+var precipServicePort = precipServiceConfig["Port"];
+
+var zipCodes = new List<string>
+{
+  "04662",
+  "14738",
+  "68023",
+  "17563",
+  "92704"
+};
+
+Console.WriteLine("Starting Data Load");
+
+var temperatureHttpClient = new HttpClient();
+temperatureHttpClient.BaseAddress = new Uri($"http://{tempServiceHost}:{tempServicePort}");
+
+var precipitationHttpClient = new HttpClient();
+precipitationHttpClient.BaseAddress = new Uri($"http://{precipServiceHost}:{precipServicePort}");
+
+foreach (var zip in zipCodes)
+{
+  Console.WriteLine($"Processing Zip Code: {zip}");
+  var from = DateTime.Now.AddYears(-2);
+  var thru = DateTime.Now;
+
+  for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+  {
+    var temps = PostTemp(zip, day, temperatureHttpClient);
+    PostPrecip(temps[0], zip, day, precipitationHttpClient);
+  }
+}
+
+void PostPrecip(int lowTemp, string zip, DateTime day, HttpClient precipitationHttpClient)
+{
+  var rand = new Random();
+  var isPrecip = rand.Next(2) < 1;
+
+  PrecipitationModel precipitation;
+  if (isPrecip)
+  {
+    var precipInches = rand.Next(1, 16);
+    if (lowTemp < 32)
+    {
+      precipitation = new PrecipitationModel
+      {
+        AmountInches = precipInches,
+        WeatherType = "snow",
+        ZipCode = zip,
+        CreatedOn = day
+      };
+    }
+    else
+    {
+      precipitation = new PrecipitationModel
+      {
+        AmountInches = precipInches,
+        WeatherType = "rain",
+        ZipCode = zip,
+        CreatedOn = day
+      };
+    }
+  }
+  else
+  {
+    precipitation = new PrecipitationModel
+    {
+      AmountInches = 0,
+      WeatherType = "none",
+      ZipCode = zip,
+      CreatedOn = day
+    };
+  }
+
+  var precipResponse = precipitationHttpClient
+    .PostAsJsonAsync("observation", precipitation)
+    .Result;
+
+  if (precipResponse.IsSuccessStatusCode)
+  {
+    Console.Write($"Posted Precipitation: Date:{day:d}" +
+                  $"Zip: {zip}" +
+                  $"Type: {precipitation.WeatherType}" +
+                  $"Amount (in.): {precipitation.AmountInches}"
+                  );
+  }
+}
+
+List<int> PostTemp(string zip, DateTime day, HttpClient temperatureHttpClient)
+{
+  var rand = new Random();
+  var t1 = rand.Next(0, 100);
+  var t2 = rand.Next(0, 100);
+  var hiLoTemps = new List<int> { t1, t2 };
+  hiLoTemps.Sort();
+
+  var temperatureObservations = new TemperatureModel
+  {
+    TempLowF = hiLoTemps[0],
+    TempHighF = hiLoTemps[1],
+    ZipCode = zip,
+    CreatedOn = day
+  };
+
+  var tempResponse = temperatureHttpClient
+      .PostAsJsonAsync("observation", temperatureObservations)
+      .Result;
+
+  if (tempResponse.IsSuccessStatusCode)
+  {
+     Console.Write($"Posted Temperature: Date:{day:d}" +
+                  $"Zip: {zip}" +
+                  $"Lo (F): {hiLoTemps[0]}" +
+                  $"hi (F): {hiLoTemps[1]}"
+                  );
+  }
+  else
+  {
+    Console.Write(tempResponse.ToString());
+  }
+
+  return hiLoTemps;
+}
