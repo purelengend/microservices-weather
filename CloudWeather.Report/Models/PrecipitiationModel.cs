bfcf0ca2f81d3b183d5759bbc0f47f0bf8884a8c
purelengend <nhathuy27301@gmail.com>
2022-12-20 09:03:26 +0700

    Add business logic for weather report service

---
+++ CloudWeather.Report/Models/PrecipitiationModel.cs
@@ -0,0 +1,7 @@
+namespace CloudWeather.Report.Models;
+
+public class PrecipitationModel
+{
+  public decimal AmountInches { get; set; }
+  public string WeatherType { get; set; }
+}
--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,4 +1,7 @@
+using CloudWeather.Report.BusinessLogic;
+using CloudWeather.Report.Config;
 using CloudWeather.Report.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,20 @@ builder.Services.AddDbContext<WeatherReportDbContext>(
     opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
   }, ServiceLifetime.Transient
 );
+
+builder.Services.AddHttpClient();
+builder.Services.AddOptions();
+builder.Services.AddTransient<IWeatherReportAggregator, WeatherReportAggregator>();
+builder.Services.Configure<WeatherDataConfig>(builder.Configuration.GetSection("WeatherDataConfig"));
+
 var app = builder.Build();
 
+app.MapGet("/weather-report/{zip}", 
+  async (string zip, [FromQuery] int? days, IWeatherReportAggregator weatherAgg) => { 
+     if (days == null || days < 0 || days > 30) 
+    return Results.BadRequest("Invalid 'days' parameter. Please provide a value from 1 to 30.");
+    var report = await weatherAgg.BuildReport(zip, days.Value);
+    return Results.Ok(report);
+  }
+);
 app.Run();
